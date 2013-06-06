685d842d2eaf8943d0db578bc303053ac0c3edbc
Jaakko Koskenkorva <jaakko@11latoa.com>
2013-06-06 23:54:48 +0300

    Initial commit

---
+++ RoskaRoope/src/main/java/com/lato/roskaroope/CameraActivity.java
@@ -0,0 +1,13 @@
+package com.lato.roskaroope;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jaakko on 6/6/13.
+ */
+public class CameraActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
