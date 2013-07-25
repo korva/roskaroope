c3f0b841e5fc867a78d37561cd366616a86a2764
Jaakko Koskenkorva <jaakko@11latoa.com>
2013-07-25 23:29:31 +0300

    Background image

--- RoskaRoope/src/main/java/com/lato/roskaroope/ScoreActivity.java
+++ RoskaRoope/src/main/java/com/lato/roskaroope/ScoreActivity.java
@@ -2,16 +2,24 @@ package com.lato.roskaroope;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 /**
  * Created by jaakko on 6/6/13.
  */
 public class ScoreActivity extends Activity {
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+
+        ImageView bg = (ImageView) findViewById(R.id.backgroundImage);
+        bg.setImageBitmap(CameraActivity.sharedCameraImage);
     }
 
     public void onContinueButtonClicked(View v) {
